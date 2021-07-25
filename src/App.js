0d787bee2174232b8256e044180c19dc733c8be9
SuryanshShahi <suryansh06shahi@gmail.com>
2021-07-26 03:27:46 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+import { Switch, Route, Redirect } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
 
-function App() {
+const App = () => {
+  // const [loading, setLoading] = useState(false);
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000);
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <section>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
 
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </section>
+  );
+};
 export default App;
+
+// {loading ? (
+//   <PropagateLoader size={20} color={"#e43c5c"} loading={loading} css={override}/>
+// ) : (   )}
---
+++ src/Data/content.js
@@ -0,0 +1,39 @@
+const Content = [
+  {
+    image: "https://rukminim1.flixcart.com/flap/128/128/image/634095dbd3751807.png?q=100",
+    text: "Mobile",
+  },
+  {
+    image: "https://rukminim1.flixcart.com/flap/128/128/image/75202e93f94951fc.png?q=100",
+    text: "Fashion",
+  },
+  {
+    image: "https://rukminim1.flixcart.com/flap/128/128/image/f9c1bab8f5391f89.png?q=100",
+    text: "Electronics",
+  },
+  {
+    image: "https://rukminim1.flixcart.com/flap/128/128/image/08eae8a37b9b570d.png?q=100",
+    text: "Home",
+  },
+  {
+    image: "https://rukminim1.flixcart.com/flap/128/128/image/bc37959077392a55.png?q=100",
+    text: "Appliances",
+  },
+  {
+    image: "https://rukminim1.flixcart.com/flap/128/128/image/ead6248cbbf91935.png?q=100",
+    text: "Travel",
+  },
+  {
+    image: "https://rukminim1.flixcart.com/flap/128/128/image/17171e66f571833d.png?q=100",
+    text: "Beauty, Toys & More",
+  },
+  {
+    image: "https://rukminim1.flixcart.com/flap/128/128/image/769f17a5217139d9.png?q=100",
+    text: "Furniture",
+  },
+  {
+    image: "https://rukminim1.flixcart.com/flap/128/128/image/60a5618cf67774f2.png?q=100",
+    text: "Grocery",
+  },
+];
+export default Content;
