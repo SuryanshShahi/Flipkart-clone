9f27790b0e2b91f4587b7709d1fa141b15c2bac7
SuryanshShahi <suryansh06shahi@gmail.com>
2021-07-27 01:36:32 +0530

    update

--- src/Data/content.js
+++ src/Data/content.js
@@ -1,39 +1,98 @@
-const Content = [
+export const Content = [
   {
-    image: "https://rukminim1.flixcart.com/flap/128/128/image/634095dbd3751807.png?q=100",
+    image:
+      "https://rukminim1.flixcart.com/flap/128/128/image/634095dbd3751807.png?q=100",
     text: "Mobile",
   },
   {
-    image: "https://rukminim1.flixcart.com/flap/128/128/image/75202e93f94951fc.png?q=100",
+    image:
+      "https://rukminim1.flixcart.com/flap/128/128/image/75202e93f94951fc.png?q=100",
     text: "Fashion",
   },
   {
-    image: "https://rukminim1.flixcart.com/flap/128/128/image/f9c1bab8f5391f89.png?q=100",
+    image:
+      "https://rukminim1.flixcart.com/flap/128/128/image/f9c1bab8f5391f89.png?q=100",
     text: "Electronics",
   },
   {
-    image: "https://rukminim1.flixcart.com/flap/128/128/image/08eae8a37b9b570d.png?q=100",
+    image:
+      "https://rukminim1.flixcart.com/flap/128/128/image/08eae8a37b9b570d.png?q=100",
     text: "Home",
   },
   {
-    image: "https://rukminim1.flixcart.com/flap/128/128/image/bc37959077392a55.png?q=100",
+    image:
+      "https://rukminim1.flixcart.com/flap/128/128/image/bc37959077392a55.png?q=100",
     text: "Appliances",
   },
   {
-    image: "https://rukminim1.flixcart.com/flap/128/128/image/ead6248cbbf91935.png?q=100",
+    image:
+      "https://rukminim1.flixcart.com/flap/128/128/image/ead6248cbbf91935.png?q=100",
     text: "Travel",
   },
   {
-    image: "https://rukminim1.flixcart.com/flap/128/128/image/17171e66f571833d.png?q=100",
+    image:
+      "https://rukminim1.flixcart.com/flap/128/128/image/17171e66f571833d.png?q=100",
     text: "Beauty, Toys & More",
   },
   {
-    image: "https://rukminim1.flixcart.com/flap/128/128/image/769f17a5217139d9.png?q=100",
+    image:
+      "https://rukminim1.flixcart.com/flap/128/128/image/769f17a5217139d9.png?q=100",
     text: "Furniture",
   },
   {
-    image: "https://rukminim1.flixcart.com/flap/128/128/image/60a5618cf67774f2.png?q=100",
+    image:
+      "https://rukminim1.flixcart.com/flap/128/128/image/60a5618cf67774f2.png?q=100",
     text: "Grocery",
   },
 ];
-export default Content;
+
+export const Banner = [
+  "https://rukminim1.flixcart.com/flap/3376/560/image/d117a62eb5fbb8e1.jpg?q=50",
+  "https://rukminim1.flixcart.com/flap/3376/560/image/57267a180af306fe.jpg?q=50",
+  "https://rukminim1.flixcart.com/flap/3376/560/image/ae9966569097a8b7.jpg?q=50",
+  "https://rukminim1.flixcart.com/flap/3376/560/image/f6202f13b6f89b03.jpg?q=50",
+];
+export const ProductLine1 = [
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/200/200/kae95e80/television/f/6/y/realme-tv-32-original-imafrz79pweqeafh.jpeg?q=70",
+    text: "Top 32 Smart Televisions",
+    price: "₹14,499",
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/200/200/khdqnbk0/computer/5/d/e/apple-original-imafxfyqkdfxqjab.jpeg?q=70",
+    text: "Best Selling Laptops",
+    price: "₹14,499",
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/200/200/klcmoi80/computer/q/e/3/na-gaming-laptop-acer-original-imagyhwfgwhkf3vv.jpeg?q=70",
+    text: "Top 32 Smart Televisions",
+    price: "₹14,499",
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/200/200/k5lcvbk0pkrrdj/hair-straightener-refurbished/u/h/p/c-bhh880-10-heated-straightening-brush-with-thermoprotect-original-imafjpg2pqgxg6yy.jpeg?q=70",
+    text: "Top 32 Smart Televisions",
+    price: "₹14,499",
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/200/200/klgx0280/mobile/h/q/e/narzo-30a-rmx3171-realme-original-imagyhbgh8qfambf.jpeg?q=70",
+    text: "Top 32 Smart Televisions",
+    price: "₹14,499",
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/200/200/klgx0280/mobile/h/q/e/narzo-30a-rmx3171-realme-original-imagyhbgh8qfambf.jpeg?q=70",
+    text: "Top 32 Smart Televisions",
+    price: "₹14,499",
+  },
+  {
+    image:
+      "https://rukminim1.flixcart.com/image/200/200/kp2y2kw0/instant-camera/e/b/4/treasure-box-mini-9-instax-mini-9-fujifilm-original-imag3efxmxfrzggf.jpeg?q=70",
+    text: "Top 32 Smart Televisions",
+    price: "₹14,499",
+  },
+];
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,57 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import web from "../images/plus.png";
 import web1 from "../images/flipkart.png";
-import Content from "../Data/content";
-// import { makeStyles } from '@material-ui/core/styles';
-// import Badge from '@material-ui/core/Badge';
-// import MailIcon from '@material-ui/icons/Mail';
+import { Content, Banner, ProductLine1 } from "../Data/content";
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button } from "@material-ui/core";
+import MultiCarousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
+var settings = {
+  indicators: false,
+  animation: "slide",
+  navButtonsAlwaysVisible: true,
+  navButtonsProps: {
+    style: {
+      background: "#fff",
+      fontSize: "2.5rem",
+      borderRadius: 0,
+      height: "104px",
+      width: "50px",
+      outline: "none",
+      color: "black",
+    },
+  },
+};
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+    slidesToSlide: 3
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+var setting = {
+  responsive: responsive,
+  centerMode: true,
+};
 function Home() {
   return (
     <section id="home">
-      <nav className="navbar navbar-expand-lg navbar-light position-sticky">
+      <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container px-5">
           <NavLink className="navbar-brand text-white pl-5" to="/">
             <img src={web1} className="img-fluid flipkart" alt="menupic"></img>
@@ -47,12 +89,21 @@ function Home() {
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
               <li className="nav-item align-items-center d-flex ml-5">
-                <NavLink className="nav-link active bg-white px-4 py-1 shadow-sm" aria-current="page" to="/">
-                  <span className="px-3 text-primary font-weight-bold">Login</span>
+                <NavLink
+                  className="nav-link active bg-white px-4 py-1 shadow-sm"
+                  aria-current="page"
+                  to="/"
+                >
+                  <span className="px-3 text-primary font-weight-bold">
+                    Login
+                  </span>
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link text-white font-weight-bold mx-4" to="/">
+                <NavLink
+                  className="nav-link text-white font-weight-bold mx-4"
+                  to="/"
+                >
                   More
                 </NavLink>
               </li>
@@ -63,8 +114,10 @@ function Home() {
                   tabindex="-1"
                   aria-disabled="true"
                 >
-                    &nbsp;<b className="" style={{fontSize:"17px"}}>Cart</b>
-
+                  &nbsp;
+                  <b className="" style={{ fontSize: "17px" }}>
+                    Cart
+                  </b>
                 </NavLink>
               </li>
             </ul>
@@ -72,19 +125,93 @@ function Home() {
         </div>
       </nav>
 
-      <nav class="navbar navbar-light bg-light shadow-sm">
+      <nav class="navbar navbar-light bg-white shadow-sm">
         <div class="container">
           {Content.map((elem) => {
             const { text, image } = elem;
             return (
-                <div className="my-3">
-                  <div className="justify-content-center d-flex"><img src={image} className="img-fluid text-center" style={{width:"65px"}}></img></div>
-                  <div className="justify-content-center d-flex mt-1"><h7 style={{fontSize:"14px"}}><b>{text}</b></h7></div>
+              <div className="my-3">
+                <div className="justify-content-center d-flex">
+                  <img
+                    src={image}
+                    className="img-fluid text-center"
+                    style={{ width: "65px" }}
+                  ></img>
                 </div>
+                <div className="justify-content-center d-flex mt-1">
+                  <h7 style={{ fontSize: "14px" }}>
+                    <b>{text}</b>
+                  </h7>
+                </div>
+              </div>
             );
           })}
         </div>
       </nav>
+
+      <div className="my-2 mx-3">
+        <Carousel {...settings} className="car">
+          {Banner.map((image) => (
+            <img src={image} className="img-fluid" alt="menupic"></img>
+          ))}
+        </Carousel>
+      </div>
+      <div className="mx-2 mr-3 mb-3">
+        <div className="container-fluid mx-1 banner2">
+          <div className="row">
+            <div className="col-10 card border-0 shadow-sm rounded-0">
+              <div className="row gx-0">
+                <div className="col-2 card border-0 rounded-0">
+                  <div className="card-body ">
+                    <div className="justify-content-center align-items-center d-flex pt-5 mt-5">
+                      <h1>Top Offers</h1>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                      <div className="btn btn-info">VIEW ALL</div>
+                    </div>
+                  </div>
+                  <div className="card-footer bg-info rounded-0">
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                  </div>
+                </div>
+                <div className="col-10">
+                  <MultiCarousel {...setting}>
+                    {ProductLine1.map((elem) => {
+                      const { text, image, price } = elem;
+                      return (
+                        <div className="card" style={{height: "100%"}}>
+                          <div className="card-body">
+                            <img
+                              src={image}
+                              className="img-fluid px-4"
+                              alt="menupic"
+                            ></img>
+                          </div>
+                          <div className="card-footer text-center" style={{fontSize: "14px"}}>
+                            <div className="font-weight-bold">{text}</div>
+                            <h5 className="text-success ">Buy Now!!</h5>
+                            <div>{price}</div>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </MultiCarousel>
+                </div>
+              </div>
+            </div>
+            <div className="col-2 shadow-sm">
+              <img
+                src="https://rukminim1.flixcart.com/flap/464/708/image/740c68de8a578ec2.jpg?q=70"
+                className="img-fluid cardH"
+                alt="menupic"
+              ></img>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
