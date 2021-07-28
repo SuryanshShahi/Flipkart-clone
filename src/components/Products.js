e24cc2f68af52b030954b64edc7e0274664aff43
SuryanshShahi <suryansh06shahi@gmail.com>
2021-07-28 20:43:17 +0530

    update

--- src/components/Products.js
+++ src/components/Products.js
@@ -26,78 +26,94 @@ function Products() {
                   <a
                     className="nav-link fsize dropdown-toggle text-dark"
                     href="#"
-                    data-toggle="dropdown"
+                    // data-toggle="dropdown1"
                   >
                     Electronics
                   </a>
+                 
                   <div className="dropdown-menu rounded-0 border-0">
-                    <div className="row ml-1 mr-2 pr-1">
-                      <div className="col-3">
-                        <b>Mobiles</b>
-                        <li>Mi</li>
-                        <li>Realme</li>
-                        <li>Samsung</li>
-                        <li>Infinix</li>
-                        <li>OPPO</li>
-                        <li>Apple</li>
-                        <li>Vivo</li>
-                        <li>Honor</li>
-                        <li>Asus</li>
-                        <li>Poco X2</li>
-                        <li>realme Narzo 10</li>
-                        <li>Infinix Hot 9</li>
-                        <li>IQOO 3</li>
-                        <li>iPhone SE</li>
-                        <li>Motorola razr</li>
-                        <li>realme Narzo 10A</li>
-                        <li>Motorola g8 power lite</li>
-                      </div>
-                      <div className="col-3">
-                        <b>Mobile Accessories</b>
-                        <li>Mobile Cases</li>
-                        <li>Headphones & Headsets</li>
-                        <li>Power Banks</li>
-                        <li>Screengaurds</li>
-                        <li>Memory Cards</li>
-                        <li>Smart Headphones</li>
-                        <li>Mobile Cables</li>
-                        <li>Mobile Charges</li>
-                        <li>Mobile Holders</li>
-                        <b>Smart Wearable Tech</b>
-                        <li>Smart Watches</li>
-                        <li>Smart Glasses</li>
-                        <li>Smart Bands</li>
-                        <b>Health Care Appliances</b>
-                        <li>Bp Monitors</li>
-                        <li>Weighing Scale</li>
-                      </div>
-                      <div className="col-3">
-                      <b>Laptops</b>  
-                      <li>Gaming Laptops</li>
-                      <b>Desktop PCs</b>
-                      <b>Gaming & Accessories</b>
-                      <b>Computer Accessories</b>
-                      <li>External Hard Disks</li>
-                      <li>Pendrives</li>
-                      <li>Laptop Skins & Decals</li>
-                      <li>Laptop Bags</li>
-                      <li>Mouse</li>
-                      <b>Computer Peripherals</b>
-                      <li>Printers & Ink Cartridges</li>
-                      <li>Monitors</li>
-                      <b>Tablets</b>
-                      <li>Apple iPads</li>
-                      </div>
-                      <div className="col-3">
-                        <li>
-                          <a className="dropdown-item" href="#">
-                            Submenu item 1
-                          </a>
-                        </li>
-                      </div>
+                  <div className="row ml-1 mr-2 pr-1">
+                    <div className="col-3">
+                      <b>Mobiles</b>
+                      <li>Mi</li>
+                      <li>Realme</li>
+                      <li>Samsung</li>
+                      <li>Infinix</li>
+                      <li>OPPO</li>
+                      <li>Apple</li>
+                      <li>Vivo</li>
+                      <li>Honor</li>
+                      <li>Asus</li>
+                      <li>Poco X2</li>
+                      <li>realme Narzo 10</li>
+                      <li>Infinix Hot 9</li>
+                      <li>IQOO 3</li>
+                      <li>iPhone SE</li>
+                      <li>Motorola razr</li>
+                      <li>realme Narzo 10A</li>
+                      <li>Motorola g8 power lite</li>
+                    </div>
+                    <div className="col-3">
+                      <b>Mobile Accessories</b>
+                      <li>Mobile Cases</li>
+                      <li>Headphones & Headsets</li>
+                      <li>Power Banks</li>
+                      <li>Screengaurds</li>
+                      <li>Memory Cards</li>
+                      <li>Smart Headphones</li>
+                      <li>Mobile Cables</li>
+                      <li>Mobile Charges</li>
+                      <li>Mobile Holders</li>
+                      <b>Smart Wearable Tech</b>
+                      <li>Smart Watches</li>
+                      <li>Smart Glasses</li>
+                      <li>Smart Bands</li>
+                      <b>Health Care Appliances</b>
+                      <li>Bp Monitors</li>
+                      <li>Weighing Scale</li>
+                    </div>
+                    <div className="col-3">
+                    <b>Laptops</b>  
+                    <li>Gaming Laptops</li>
+                    <b>Desktop PCs</b>
+                    <b>Gaming & Accessories</b>
+                    <b>Computer Accessories</b>
+                    <li>External Hard Disks</li>
+                    <li>Pendrives</li>
+                    <li>Laptop Skins & Decals</li>
+                    <li>Laptop Bags</li>
+                    <li>Mouse</li>
+                    <b>Computer Peripherals</b>
+                    <li>Printers & Ink Cartridges</li>
+                    <li>Monitors</li>
+                    <b>Tablets</b>
+                    <li>Apple iPads</li>
+                    </div>
+                    <div className="col-3">
+                      <li>Televisions</li>
+                      <li><b>Speakers</b></li>
+                      <li>Home Audio Speakers</li>
+                      <li>Home Threaters</li>
+                      <li>Soundbars</li>
+                      <li>Bluetooth Speakers</li>
+                      <li>DTH Set Top Box</li>
+                      <li><b>Smart Home Automation</b></li>
+                      <li>Google Nest</li>
+                      <li><b>Camera</b></li>
+                      <li>DSLR & Mirrorless</li>
+                      <li>Compact & Bridge Cameras</li>
+                      <li>Sports & Action</li>
+                      <li><b>Camera Accessories</b></li>
+                      <li>Lens</li>
+                      <li>Tripods</li>
+                      <li><b>Network Components</b></li>
+                      <li>Routers</li>
                     </div>
                   </div>
-                </li>
+                </div>
+              
+                 
+                  </li>
                 <li className="nav-item mx-5 dropdown">
                   <a
                     className="nav-link fsize dropdown-toggle text-dark"
@@ -106,7 +122,7 @@ function Products() {
                   >
                     Electronics
                   </a>
-                  <div className="dropdown-menu rounded-0 border-0">
+                  <div className="dropdown-menu menu1 rounded-0 border-0">
                     <div className="row ml-1 mr-2 pr-1">
                       <div className="col-3">
                         <li>
@@ -147,7 +163,7 @@ function Products() {
                   >
                     Electronics
                   </a>
-                  <div className="dropdown-menu rounded-0 border-0">
+                  <div className="dropdown-menu menu2 rounded-0 border-0">
                     <div className="row ml-1 mr-2 pr-1">
                       <div className="col-3">
                         <li>
@@ -188,7 +204,7 @@ function Products() {
                   >
                     Electronics
                   </a>
-                  <div className="dropdown-menu rounded-0 border-0">
+                  <div className="dropdown-menu menu3 rounded-0 border-0">
                     <div className="row ml-1 mr-2 pr-1">
                       <div className="col-3">
                         <li>
@@ -221,6 +237,9 @@ function Products() {
                     </div>
                   </div>
                 </li>
+              
+              
+                
               </ul>
             </div>
           </div>
